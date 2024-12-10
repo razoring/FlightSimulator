7aee1ed7b5ff9be311b685e9a8ab4f72ea375c0e
JW <166519339+Scientific-Railgun@users.noreply.github.com>
2024-12-10 14:54:52 -0500

    PlaneUI

---
+++ PlaneUI_Test.java
@@ -0,0 +1,13 @@
+// Testing Frame
+package FlightSimulator;
+
+import javax.swing.JFrame;
+public class PlaneUI_Test
+{
+	public static void main( String args[] ) {
+		PlaneUI planeUI = new PlaneUI();
+		planeUI.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		planeUI.setSize( 350, 200 ); // set frame size
+		planeUI.setVisible( true ); // display frame
+	} // end main
+} // end class FrameTest
