945cf0f4ae85643a217ba6f7374a54a43b2a3f57
razoring <blotzytgaming@gmail.com>
2024-12-13 15:02:15 -0500

    plane

--- PlaneUI_Test.java
+++ PlaneUI_Test.java
@@ -6,7 +6,7 @@ public class PlaneUI_Test {
 	public static void main( String args[] ) {
 		PlaneUI planeUI = new PlaneUI();
 		planeUI.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		planeUI.setSize( 400, 400 ); // set frame size
+		planeUI.setSize( 500, 500 ); // set frame size
 		planeUI.setVisible( true ); // display frame
 	} // end main
 } // end class FrameTest
