655621e2cbd514c2e185cc7049bffe70aec5bd07
JW <166519339+Scientific-Railgun@users.noreply.github.com>
2024-12-16 15:02:03 -0500

    Update Controller.java
    + Code comments

--- Controller.java
+++ Controller.java
@@ -7,11 +7,10 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.io.File;
 
-/*
- * Raymond So
- * 12/10/24
- * Backend
- * 2 in, 1 out
+/**
+ * Raymond So <p>
+ * 12/10/24 <p>
+ * Backend | 2 in, 1 out
  */
 
 public class Controller {
@@ -19,22 +18,33 @@ public class Controller {
 	public Queue<String> outbound = new LinkedList<String>();
 	public boolean handling = false; // debounce
 	public boolean started = false; // handle simulation init
-	
+
+	/**
+	 * Adds a flight number to inbound flights
+	 */
 	public void addIn(String plane) {
 		inbound.add(plane);
 		//System.out.println("in");
 	}
 
+	/**
+	 * Adds a flight number to outbound flights
+	 */
 	public void addOut(String plane) {
 		outbound.add(plane);
 		//System.out.println("out");
 	}
-	
+	/**
+	 * Prints string representation of all inbound & outbound planes
+	 */
 	@Override
 	public String toString() {
 		return "Inbound: " + Arrays.deepToString(inbound.toArray()) + " Outbound: " + Arrays.deepToString(outbound.toArray());
 	}
-	
+
+	/**
+	 * Removes the specified flight from queue
+	 */
 	public void updateQueue(Queue q) {
 		q.remove(); // removes flight number
 		PlaneUIApp.app.refreshList(); // updates display
