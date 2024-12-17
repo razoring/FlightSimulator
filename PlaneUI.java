cf75749517a00f62b4304422446849bfa3a34715
JW <166519339+Scientific-Railgun@users.noreply.github.com>
2024-12-17 14:19:59 -0500

    Update PlaneUI.java

--- PlaneUI.java
+++ PlaneUI.java
@@ -12,7 +12,6 @@ import java.util.Arrays;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
